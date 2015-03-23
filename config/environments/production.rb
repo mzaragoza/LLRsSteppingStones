c9ea0cac0d1e630e92ce8a088a84d7474cc07739
Moises Zaragoza <mZaragoza@circletechfl.com>
2015-03-23 13:54:46 -0400

    production environments

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -21,7 +21,7 @@ Rails.application.configure do
 
   # Disable Rails's static asset server (Apache or nginx will already do this).
   #config.serve_static_assets = false
-  config.serve_static_files = true
+  config.serve_static_assets = true
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   config.force_ssl = false
